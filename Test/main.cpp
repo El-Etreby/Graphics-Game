f527310f3c8311260798c5c25052e04f23fe1c14
El-Etreby <omarezzatel-etreby@Omars-MacBook-Pro-7.local>
2017-10-07 02:59:06 +0200

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Omar Ezzat El-Etreby on 10/7/17.
+//  Copyright © 2017 Etro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
